5d48add6dd050b6c335c4ef98eaded758bad757e
João Marcos <matiaseeepocs@gmail.com>
2024-01-31 01:27:49 -0300

    Url bd

--- lib/dados/bd_livros.dart
+++ lib/dados/bd_livros.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 
 class BdLivros extends ChangeNotifier {
   final List<Livro> _bdLivros = [];
-  final String url = '';
+  final String url = 'testando';
   List<Livro> get bdLivros {
     return _bdLivros;
   }
