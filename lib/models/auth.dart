e6e1ced096d4669ff81d71514c5e9cdfa4f9bcdf
João Marcos <matiaseeepocs@gmail.com>
2024-02-29 16:33:40 -0300

    login

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -2,15 +2,16 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:guialeitura/dados/bd.dart';
 import 'package:guialeitura/utils/routes.dart';
 import 'package:http/http.dart';
 
 class Auth extends ChangeNotifier{
 
- Future<void> _authenticate(BuildContext context, String email, String senha, String composicao) async{
+ Future<Response> _authenticate(BuildContext context, String email, String senha, String composicao) async{
 
-  String authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:$composicao?key=AIzaSyDgJrvso7mQad2EhVoZkv8_imlGdfWCK4A';
-  final response = await post(
+  String authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:$composicao?${Bd().urlKeyAuth}';
+  var response = await post(
     Uri.parse(authUrl),
     body: ({
       'email': email,
@@ -18,25 +19,27 @@ class Auth extends ChangeNotifier{
       'returnSecureToken': "true",
     }),
    
-  ).then((value) {
-   var dados = jsonDecode(value.body);
-    debugPrint(dados['error'].toString());
+  );
+  return response;
+  
 
-    if(dados['error'] == null){
-      Navigator.of(context).pushNamed(RoutesPage().INICIO);
-    }
-  });
-  debugPrint(response);
  }  
 
   Future<void> signup(BuildContext context,String email, String senha) async{
     debugPrint('Chamando cadastro');
-     await _authenticate(context,email, senha, 'signUp');
+     await _authenticate(context,email, senha, 'signUp').then((value) => null);
   }
 
  Future<void> signin(BuildContext context, email, String senha) async {
     debugPrint('Chamando login');
-  await _authenticate(context, email, senha, 'signInWithPassword').then((value) => null);
+  await _authenticate(context, email, senha, 'signInWithPassword').then((value) {
+      var dados = jsonDecode(value.body);
+      debugPrint(dados['error'].toString());
+
+      if (dados['error'] == null) {
+        Navigator.of(context).pushNamed(RoutesPage().INICIO);
+      }
+    });
 }
 
 
