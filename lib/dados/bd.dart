cbc62b12b8fb153da2d1c7287dc7bd51046ae17d
João Marcos <matiaseeepocs@gmail.com>
2024-03-27 09:41:27 -0300

    Atualizacao gradle

---
+++ lib/dados/bd.dart
@@ -0,0 +1,7 @@
+class Bd{
+
+
+  final String urlBd = 'https://guialeitura-7f3b5-default-rtdb.firebaseio.com';
+  final String urlKeyAuth = 'key=AIzaSyDgJrvso7mQad2EhVoZkv8_imlGdfWCK4A';
+
+}
--- lib/dados/bd_livros.dart
+++ lib/dados/bd_livros.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:guialeitura/dados/bd.dart';
 import 'package:guialeitura/models/livro.dart';
