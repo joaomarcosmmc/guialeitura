6d696d909943ad7fe4ed28ac4343c1d83f5bc344
João Marcos <matiaseeepocs@gmail.com>
2024-03-28 08:04:02 -0300

    page statist

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:guialeitura/dados/bd_livros.dart';
 import 'package:guialeitura/dados/auth.dart';
 import 'package:guialeitura/pages/auth_or_home_page.dart';
 import 'package:guialeitura/pages/livros_detalhes_page.dart';
-
 import 'package:guialeitura/utils/routes.dart';
 import 'package:provider/provider.dart';
 
